fb4d04e97e328a9c3afc2b2e2113367ec33658a1
Maria Parusk <maria.parusk@estcube.eu>
2020-10-26 12:16:13 +0200

    Changes

--- src/main/bot.py
+++ src/main/bot.py
@@ -8,7 +8,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-
+# Prints out the result when the Client (representation of the connection to Discord) has connected to Discord.
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
